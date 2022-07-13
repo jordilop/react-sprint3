24e1b5eb85d375ea4aa1c441f266957e4af13f57
Jordi <jordilop@gmail.com>
2022-07-13 02:51:31 +0200

    Exercici 4

--- js/shop.js
+++ js/shop.js
@@ -98,6 +98,16 @@ function calculateTotal() {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    cartList.forEach(function(product) {
+        let index = cart.findIndex(products => products.id == product.id);
+        if(index == -1) {
+            cart.push({...product, quantity: 1});
+        } else {
+            cart[index].quantity++;
+        }
+    });
+    
+    cleanCart();
 }
 
 // Exercise 5
